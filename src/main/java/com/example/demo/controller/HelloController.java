0b014c6e4953b522934a3b73dd7cd399e1f9bbab
Jiajie Wang <wjj12393@163.com>
2026-08-24 13:45:30 +0800

    Add HelloController with hello endpoint

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+
+    public String hello(){
+
+
+        return "Hello World";
+
+    }
+
+}
